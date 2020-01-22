9b478ed14d471794b4b9aa8e89ba215e3f0de09a
TUMELO38 <TumeloM2@discovery.co.za>
2020-01-22 21:19:17 +0200

    Added user types for all constants

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import './App.css';
 import {setCurrentUser} from "./redux/user/user.actions";
 
@@ -46,7 +46,15 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
-                    <Route path='/signin' component={SignInAndSignUpPage} />
+                    <Route
+                        exact path='/signin'
+                        render={
+                            () => this.props.currentUser ?
+                                (<Redirect to='/'/>)
+                                :
+                                (<SignInAndSignUpPage/>)
+                        }
+                    />
                 </Switch>
             </div>
         );
@@ -54,7 +62,11 @@ class App extends React.Component {
 
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch =>({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
-export default connect(null, mapDispatchToProps) (App);
+export default connect(mapStateToProps, mapDispatchToProps) (App);
